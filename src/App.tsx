fbf1c0efc7f384f2bf5cc6f008ca5493d7bf2834
Agataandrade <agatasouandrade@gmail.com>
2025-09-15 12:51:25 -0300

    feat: adiciona navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-// import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Home from "./pages/home/Home";
-import Cadastro from "./pages/cadastro/Cadastro";
+import Navbar from "./components/navbar/Navbar.tsx";
+
 
 function App() {
 	return (
 		<>
 			<AuthProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/home" element={<Home />} />
-						<Route path="/cadastro" element={<Cadastro />} />
-					</Routes>
+				<Navbar />
+					
 				</BrowserRouter>
 			</AuthProvider>
 		</>
